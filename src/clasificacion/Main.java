4e1fb52eb90acd6a201bc83738c0e74017292849
Diego Sanchez <diegosanchez93@outlook.com>
2015-02-10 15:19:49 -0400

    Creando la estructura del proyecto

--- src/clasificacion/Main.java
+++ src/clasificacion/Main.java
@@ -18,7 +18,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
         //
-        WekaTest.main(args);
+        //WekaTest.main(args);
     }
     
 }
